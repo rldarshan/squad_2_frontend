f260abed63ede3053a78546903832a27e5b27878
nethra.govinda <nethragovinda@gmail.com>
2025-01-10 18:40:52 +0530

    Final Frontend Commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/register" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/doctordashboard" element={<DoctorDashboard />} />
